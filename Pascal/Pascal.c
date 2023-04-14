615a48e02219dc1c6d798d902e74e8cfe6d06dac
e2202769 <e2202769@WA2029-02>
2023-04-14 15:10:34 +0300

    Add project files.

---
+++ Pascal/Pascal.c
@@ -0,0 +1,10 @@
+#include "Pascal.h"
+
+int binomial(int n, int k) {
+	return factorial(n) / (factorial(k) * factorial(n - k));
+}
+
+int factorial(int n) {
+	if (n == 0) return 1;
+	return n * factorial(n - 1);
+}
---
+++ Pascal/Pascal.h
@@ -0,0 +1,12 @@
+
+#pragma once
+//usually are 3 things to be stored in a header file
+// 1. Constant definitions
+#define ROWS 11
+
+// 2. Definition of data structures
+
+// 3. Function declararations 
+int binomial(int n, int k);
+int factorial(int n);
+
